2036b115e21ccc85184410cb8e09924b459deff0
BelatedMussel <38633896+BelatedMussel@users.noreply.github.com>
2018-06-02 08:55:26 -0600

    Fixed typos and a syntax error in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='minie',
       version='0.1',
       description='A simple HTML text editor',
       url='https://github.com/BelatedMussel/minie',
       author='Joshua Kurtenbach',
-      author_email=:'38633896+BelatedMussel@users.noreply.github.com',
+      author_email='38633896+BelatedMussel@users.noreply.github.com',
       license='GPLv3',
-      packages=['minie'],
+      packages=find_packages(),
       install_requires=[
-          'SidePy',
-          'sys',
-          'ext',
+          'PySide',
           ]
       )
