767f6fc85b9b3f36669a2445b8e9a6eb17747e5e
BelatedMussel <38633896+BelatedMussel@users.noreply.github.com>
2018-06-01 22:05:12 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='minie',
       version='0.1',
       description='A simple HTML text editor',
-      url='https://github.com/BelatedMussel',
+      url='https://github.com/BelatedMussel/minie',
       author='Joshua Kurtenbach',
-      author_email='',
+      author_email=:'38633896+BelatedMussel@users.noreply.github.com',
       license='GPLv3',
       packages=['minie'],
       install_requires=[
